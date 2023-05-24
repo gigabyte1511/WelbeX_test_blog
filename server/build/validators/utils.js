ff89e8197409a2753b99c1bfaf9b53d0ebcb2fd6
Denis Gabets <gigabyte1511@gmail.com>
2023-05-24 13:06:25 +0600

    Delete server/build directory

--- server/build/validators/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPreparedErrorsFromYup = void 0;
-var getPreparedErrorsFromYup = function (e) { return e.inner.reduce(function (acc, el) {
-    acc[el.path] = el.errors.join(', ');
-    return acc;
-}, {}); };
-exports.getPreparedErrorsFromYup = getPreparedErrorsFromYup;
